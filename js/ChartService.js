6a9c60320beefb52d8b513b332e1990ff92b86bb
Jessica Sena <jessisena@gmail.com>
2018-03-14 01:04:19 +0100

    Subimos datos de lima

--- js/ChartService.js
+++ js/ChartService.js
@@ -6,6 +6,8 @@ function ChartService(){
         $("#panel-chart").removeClass("invisible");
 
         var ctx = document.getElementById('chart-area').getContext('2d');
+        var ctxBar = document.getElementById('bar-area').getContext('2d');
+
         var config = {
             type: 'doughnut',
             data: {
@@ -39,7 +41,48 @@ function ChartService(){
                 }
             }
         };
-		window.myDoughnut = new Chart(ctx, config);
+
+        var configBar = {
+            type: 'bar',
+            data: {
+                labels: ['Links a Wikipedia'],
+                datasets: [
+                    {
+                        label: '% con link',
+                        stack: 'Stack 0',
+                        backgroundColor: '#FFCA3A',
+				        borderColor: '#FFCA3A',
+                        borderWidth: 1,
+                        data: [datos.pcLink]
+                    },
+                    {
+                        label: '% sin link',
+                        stack: 'Stack 0',
+                        backgroundColor: '#D6D6D6',
+				        borderColor: '#D6D6D6',
+                        borderWidth: 1,
+                        data: [datos.pcNoLink]
+                    }                    
+                ]
+
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: 'Calles de Mujer con link a Wikipedia'
+                },
+                barThickness: 10,
+                maxBarThickness: 10,
+                responsive: true
+            }
+        };
+
+        window.myDoughnut = new Chart(ctx, config);
+        //window.myBar = new Chart(ctxBar, configBar);
     };
+
+  
+
+
 
 }
