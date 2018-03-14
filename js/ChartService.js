61cce42e3b9d226212d13d951dc53f4495a781f0
Jessica Sena <jessisena@gmail.com>
2018-03-14 23:21:06 +0100

    Añadimos gráficos wikipedia

--- js/ChartService.js
+++ js/ChartService.js
@@ -6,7 +6,7 @@ function ChartService(){
         $("#panel-chart").removeClass("invisible");
 
         var ctx = document.getElementById('chart-area').getContext('2d');
-        //var ctxBar = document.getElementById('bar-area').getContext('2d');
+        var ctxPie = document.getElementById('pie-area').getContext('2d');
 
         var config = {
             type: 'doughnut',
@@ -42,43 +42,43 @@ function ChartService(){
             }
         };
 
-        var configBar = {
-            type: 'bar',
+        var configPie = {
+            type: 'pie',
             data: {
-                labels: ['Links a Wikipedia'],
-                datasets: [
-                    {
-                        label: '% con link',
-                        stack: 'Stack 0',
-                        backgroundColor: '#FFCA3A',
-				        borderColor: '#FFCA3A',
-                        borderWidth: 1,
-                        data: [datos.pcLink]
-                    },
-                    {
-                        label: '% sin link',
-                        stack: 'Stack 0',
-                        backgroundColor: '#D6D6D6',
-				        borderColor: '#D6D6D6',
-                        borderWidth: 1,
-                        data: [datos.pcNoLink]
-                    }                    
-                ]
-
+                datasets: [{
+                    data: [ datos.numLink, datos.numNoLink],
+                    backgroundColor: [
+                       
+                       '#FFCA3A'
+                    ],
+                    label: 'label'
+                }],
+                labels: [ datos.pcLink+'% tiene', datos.pcNoLink+'% no tiene' ]
             },
             options: {
+                responsive: true,
+                legend: {
+                    position: 'bottom',
+                    fontFamily: 'Roboto',
+                    fullWidth: false
+                },
                 title: {
                     display: true,
-                    text: 'Calles de Mujer con link a Wikipedia'
-                },
-                barThickness: 10,
-                maxBarThickness: 10,
-                responsive: true
+                    text: 'Calles de Mujer con artículo en Wikipedia',
+                    position: 'top',
+                    fontFamily: 'Roboto',
+                    fontSize: 14
+                },            
+                animation: {
+                    animateScale: true,
+                    animateRotate: true
+                }
             }
         };
 
+
         window.myDoughnut = new Chart(ctx, config);
-        //window.myBar = new Chart(ctxBar, configBar);
+        window.myBar = new Chart(ctxPie, configPie);
     };
 
   
--- js/index.js
+++ js/index.js
@@ -26,7 +26,8 @@ function initApp(){
         $(".nombre-ciudad").removeClass("selected");
         $(this).addClass("selected");
         myMap.mapTo(constats.centerList.barcelona);        
-        chartService.loadChart(constats.datos.barcelona, 'Barcelona');  
+        chartService.loadChart(constats.datos.barcelona, 'Barcelona'); 
+        showChart();
     });
     $('#menu-barcelona').click(function() {        
         $("#menu-list a").removeClass("menu-selected");
@@ -35,6 +36,7 @@ function initApp(){
 
         myMap.mapTo(constats.centerList.barcelona);        
         chartService.loadChart(constats.datos.barcelona, 'Barcelona');  
+        $('#open-chart-btn').addClass("invisible");
     });
    
 
@@ -43,12 +45,14 @@ function initApp(){
         $(this).addClass("selected"); 
         myMap.mapTo(constats.centerList.asuncion);        
         chartService.loadChart(constats.datos.asuncion, 'Asunción'); 
+        $('#open-chart-btn').addClass("invisible");
     });
     $('#menu-asuncion').click(function() {
         $("#menu-list a").removeClass("menu-selected");
         $(this).addClass("menu-selected");
         myMap.mapTo(constats.centerList.asuncion);        
         chartService.loadChart(constats.datos.asuncion, 'Asunción'); 
+        $('#open-chart-btn').addClass("invisible");
         $(".navbar-collapse").removeClass("show");
     });
  
@@ -57,14 +61,16 @@ function initApp(){
         $(".nombre-ciudad").removeClass("selected");
         $(this).addClass("selected"); 
         myMap.mapTo(constats.centerList.buenosaires);        
-        chartService.loadChart(constats.datos.buenosaires, 'Buenos Aires');     
+        chartService.loadChart(constats.datos.buenosaires, 'Buenos Aires');   
+        showChart(); 
     });
     $('#menu-buenosaires').click(function() {
         $("#menu-list a").removeClass("menu-selected");
         $(this).addClass("menu-selected");
         $(".navbar-collapse").removeClass("show");
         myMap.mapTo(constats.centerList.buenosaires);        
-        chartService.loadChart(constats.datos.buenosaires, 'Buenos Aires');         
+        chartService.loadChart(constats.datos.buenosaires, 'Buenos Aires');  
+        showChart();      
     });
 
     $("#cochabamba" ).click(function() {
@@ -72,12 +78,14 @@ function initApp(){
         $(this).addClass("selected"); 
         myMap.mapTo(constats.centerList.cochabamba);        
         chartService.loadChart(constats.datos.cochabamba, 'Cochabamba'); 
+        $('#open-chart-btn').addClass("invisible");
     });
     $('#menu-cochabamba').click(function() {
         $("#menu-list a").removeClass("menu-selected");
         $(this).addClass("menu-selected");
         myMap.mapTo(constats.centerList.cochabamba);        
-        chartService.loadChart(constats.datos.cochabamba, 'Cochabamba');      
+        chartService.loadChart(constats.datos.cochabamba, 'Cochabamba');     
+        showChart();
         $(".navbar-collapse").removeClass("show");
     });
      
@@ -86,14 +94,16 @@ function initApp(){
         $(".nombre-ciudad").removeClass("selected");
         $(this).addClass("selected"); 
         myMap.mapTo(constats.centerList.lima);        
-        chartService.loadChart(constats.datos.lima, 'Lima');  
+        chartService.loadChart(constats.datos.lima, 'Lima'); 
+        showChart();
 
     });
     $('#menu-lima').click(function() {
         $("#menu-list a").removeClass("menu-selected");
         $(this).addClass("menu-selected");
         myMap.mapTo(constats.centerList.lima);        
-        chartService.loadChart(constats.datos.lima, 'Lima');  
+        chartService.loadChart(constats.datos.lima, 'Lima'); 
+        showChart();
         $(".navbar-collapse").removeClass("show");
     });
  
@@ -103,14 +113,38 @@ function initApp(){
         $(this).addClass("selected");
         myMap.mapTo(constats.centerList.montevideo);        
         chartService.loadChart(constats.datos.montevideo, 'Montevideo'); 
+        showChart();
     });
     $('#menu-montevideo').click(function() {
         $("#menu-list a").removeClass("menu-selected");
         $(this).addClass("menu-selected");
         myMap.mapTo(constats.centerList.montevideo);        
-        chartService.loadChart(constats.datos.montevideo, 'Montevideo'); 
+        chartService.loadChart(constats.datos.montevideo, 'Montevideo');
+        showChart();
         $(".navbar-collapse").removeClass("show");
     });
+
+    //** CHART display*/
+
+    $('#close-chart-btn').click(function(){
+        hideChart();
+    });
+
+    $('#open-chart-btn').click(function(){
+        showChart();
+    });
+    
+    function hideChart(){
+        $("#panel-chart").addClass("animated slideOutDown");
+        $('#open-chart-btn').removeClass("invisible animated slideOutDown");
+        $("#open-chart-btn").addClass("animated slideInUp");
+    }
+    
+    function showChart(){
+        $('#panel-chart').removeClass("animated slideOutDown");
+        $("#panel-chart").addClass("animated slideInUp");
+        $("#open-chart-btn").addClass("animated slideOutDown");
+    }
        
 }
 
