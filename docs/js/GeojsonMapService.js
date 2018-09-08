90e7e07261bdba832f215ccf9944b6063609aec7
betCG <betzychaves@gmail.com>
2018-09-07 18:21:58 -0600

    Se modifica el css para agregar nombre-pais

--- docs/js/GeojsonMapService.js
+++ docs/js/GeojsonMapService.js
@@ -1,7 +1,7 @@
 function GeojsonMapService(){
 
     //this.map = map;
-    this.urlData = 'https://raw.githubusercontent.com/geochicasosm/lascallesdelasmujeres/master';
+    this.urlData = 'https://raw.githubusercontent.com/betCG/lascallesdelasmujeres/master';
 
     this.loadGeojson = function(map, folder, isMobile, coords){    
     
--- docs/js/index.js
+++ docs/js/index.js
@@ -8,7 +8,7 @@ function initApp(){
     const myMap = new MyMap();
     const isMobile = isMobileDevice();
     let openToggleMenu = false;
- 
+
     const menuListELem = document.getElementById("menu-list");
     const panelListELem = document.getElementById("ciudades-list");
 
@@ -19,90 +19,121 @@ function initApp(){
         selectedCity = window.location.hash.replace('#', '');
     }
     // Sort cities alphabetically
-    constants.citiesList.sort(function (a, b) {
+/*    constants.countriesList.sort(function (a, b) {
         let nameA = a.name.toLowerCase();
         let nameB = b.name.toLowerCase();
         if (nameA < nameB) return -1;
         if (nameA > nameB) return 1;
         return 0;
-    });
-    // Create an HTML node for every city and load its data
-    for(let i= 0; i<constants.citiesList.length; i++){
-
-        const city = constants.citiesList[i];
-
-        myMap.map.on('load', geojsonMapService.loadGeojson(myMap.map, city.id, isMobile, city.center));
-        
-        const elem = document.createElement("DIV");        
-        const elemMenu = document.createElement("A");
+    });*/
 
 
-        elem.setAttribute("id", city.id);
-        elem.classList.add("nombre-ciudad");
-        elem.setAttribute("href", "#" + city.id);
+    for (let j=0; j<constants.countriesList.length; j++){
+        const country = constants.countriesList[j];
 
-        elemMenu.setAttribute("id", "menu-"+ city.id);
-        elemMenu.setAttribute("href", "#" + city.id);
-        elemMenu.classList.add("nav-item", "nav-link",  "my-menu-item");                        
+        const elemC = document.createElement("DIV");
+        const elemMenuC = document.createElement("A");
 
-        elem.appendChild(document.createTextNode(city.name.toLocaleUpperCase()));
-        elemMenu.appendChild(document.createTextNode(city.name.toLocaleUpperCase()));
+        elemC.setAttribute("id",country.id);
+        elemC.classList.add("nombre-pais");
+        elemC.setAttribute("href", "#" + country.id);
 
-        /*CLICK EVENTS*/
+        elemMenuC.setAttribute("id", "menu-"+ country.id);
+        elemMenuC.setAttribute("href", "#" + country.id);
+        elemMenuC.classList.add("nav-item", "nav-link",  "my-menu-item");
 
-        elem.onclick = doElemClick;
-        function doElemClick() {
-            
-            const prevElem = document.getElementsByClassName("selected");
-            if(prevElem.length !== 0) prevElem[0].classList.remove("selected");
-            elem.classList.add("selected");
 
-            myMap.mapTo(city.center);        
-            chartService.loadChart(city.datos, city.name); 
-            showChart();
+        elemC.appendChild(document.createTextNode(country.name.toLocaleUpperCase()));
+        elemMenuC.appendChild(document.createTextNode(country.name.toLocaleUpperCase()));
+
+        panelListELem.appendChild(elemC);
+        menuListELem.appendChild(elemMenuC);
+
+
+        // Create an HTML node for every city and load its data
+        for(let i= 0; i<country.citiesList.length; i++){
+
+            const city = country.citiesList[i];
+
+            myMap.map.on('load', geojsonMapService.loadGeojson(myMap.map, city.id, isMobile, city.center));
+
+            const elem = document.createElement("DIV");
+            const elemMenu = document.createElement("A");
+
+            elem.setAttribute("id", city.id);
+            elem.classList.add("nombre-ciudad");
+            elem.setAttribute("href", "#" + city.id);
+
+            elemMenu.setAttribute("id", "menu-"+ city.id);
+            elemMenu.setAttribute("href", "#" + city.id);
+            elemMenu.classList.add("nav-item", "nav-link",  "my-menu-item");
+
+            elem.addEventListener("mouseenter", addBackgroundColor);
+            elemMenu.addEventListener("mouseenter", addBackgroundColor);
+
+            elem.addEventListener("mouseout", removeBackgroundColor, false);
+            elemMenu.addEventListener("mouseout", removeBackgroundColor, false);
+
+            elem.appendChild(document.createTextNode(city.name.toLocaleUpperCase()));
+            elemMenu.appendChild(document.createTextNode(city.name.toLocaleUpperCase()));
+
+            /*CLICK EVENTS*/
+
+            elem.onclick = doElemClick;
+            function doElemClick() {
+
+                const prevElem = document.getElementsByClassName("selected");
+                if(prevElem.length !== 0) prevElem[0].classList.remove("selected");
+                elem.classList.add("selected");
+
+                myMap.mapTo(city.center);
+                chartService.loadChart(city.datos, city.name);
+                showChart();
+            }
+
+            elemMenu.onclick = doElemMenuClick;
+            function doElemMenuClick() {
+
+                const prevElemMenu = document.getElementsByClassName("menu-selected");
+                if(prevElemMenu.length !== 0) prevElemMenu[0].classList.remove("menu-selected");
+                elemMenu.classList.add("menu-selected");
+
+                const navBar =  document.getElementsByClassName("navbar-collapse");
+                if(navBar.length !== 0) navBar[0].classList.remove("show");
+
+                const icon = document.getElementById("menu-toggle-icon");
+                icon.classList.remove("fa-chevron-circle-right");
+                icon.classList.add("fa-chevron-circle-down");
+                openToggleMenu = false;
+
+                myMap.mapTo(city.center);
+                chartService.loadChart(city.datos, city.name);
+                showChart();
+            }
+
+            elem.addEventListener("mouseenter", addBackgroundColor);
+            elemMenu.addEventListener("mouseenter", addBackgroundColor);
+
+            elem.addEventListener("mouseout", removeBackgroundColor, false);
+            elemMenu.addEventListener("mouseout", removeBackgroundColor, false);
+
+            //Add elem to the panel and menu list
+            elemC.appendChild(elem);
+            elemMenuC.appendChild(elemMenu);
+
+            if (selectedCity === city.id) {
+                elem.classList.add("selected");
+                elemMenu.classList.add("menu-selected");
+                // Load the selected city
+                myMap.mapTo(city.center);
+                chartService.loadChart(city.datos, city.name);
+                showChart();
+            }
+
         }
 
-        elemMenu.onclick = doElemMenuClick;
-        function doElemMenuClick() {
 
-            const prevElemMenu = document.getElementsByClassName("menu-selected");
-            if(prevElemMenu.length !== 0) prevElemMenu[0].classList.remove("menu-selected");
-            elemMenu.classList.add("menu-selected");
-
-            const navBar =  document.getElementsByClassName("navbar-collapse");
-            if(navBar.length !== 0) navBar[0].classList.remove("show");
-
-            const icon = document.getElementById("menu-toggle-icon");
-            icon.classList.remove("fa-chevron-circle-right");
-            icon.classList.add("fa-chevron-circle-down");
-            openToggleMenu = false;
-
-            myMap.mapTo(city.center);        
-            chartService.loadChart(city.datos, city.name);  
-            showChart();
-        }
-
-        elem.addEventListener("mouseenter", addBackgroundColor);
-        elemMenu.addEventListener("mouseenter", addBackgroundColor);
-        
-        elem.addEventListener("mouseout", removeBackgroundColor, false);
-        elemMenu.addEventListener("mouseout", removeBackgroundColor, false);
-
-        //Add elem to the panel and menu list
-        panelListELem.appendChild(elem);
-        menuListELem.appendChild(elemMenu);
-
-        if (selectedCity === city.id) {
-            elem.classList.add("selected");
-            elemMenu.classList.add("menu-selected");
-            // Load the selected city
-            myMap.mapTo(city.center);        
-            chartService.loadChart(city.datos, city.name);  
-            showChart();
-        }
-
-    }
-
+      }
 
     function addBackgroundColor(event){
         event.target.style.backgroundColor = "#243342";
@@ -112,8 +143,8 @@ function initApp(){
     }
 
     const toggleMenu = document.getElementById("my-toggle-menu");
-    toggleMenu.addEventListener("click", updateToggleMenuIcon); 
-   
+    toggleMenu.addEventListener("click", updateToggleMenuIcon);
+
 
     //** CHART display*/
     const closeChartBtn = document.getElementById('close-chart-btn');
@@ -122,21 +153,21 @@ function initApp(){
 
     closeChartBtn.onclick = hideChart;
     openChartBtn.onclick = showChart;
-    
+
     function hideChart(){
-        
+
         panelChart.classList.add("animated", "fadeOut");
         panelChart.style.zIndex = "-1";
         openChartBtn.classList.remove("invisible", "animated", "fadeOut");
         openChartBtn.classList.add("animated", "fadeIn");
 
     }
-    
+
     function showChart(){
 
         panelChart.classList.remove("animated", "fadeOut");
         panelChart.classList.add("animated", "fadeIn");
-        panelChart.style.zIndex = "999";        
+        panelChart.style.zIndex = "999";
         openChartBtn.classList.add("animated", "fadeOut");
 
     }
@@ -146,10 +177,10 @@ function initApp(){
     }
 
     function updateToggleMenuIcon(){
-        
+
         const icon = document.getElementById("menu-toggle-icon");
 
-        if(!openToggleMenu){            
+        if(!openToggleMenu){
             icon.classList.remove("fa-chevron-circle-down");
             icon.classList.add("fa-chevron-circle-right");
         } else {
@@ -161,11 +192,11 @@ function initApp(){
 
     function addText() {
         const userLang = navigator.language || navigator.userLanguage;
-        
+
 
     }
-    
-       
+
+
 }
 
 
@@ -175,4 +206,3 @@ window.onload = function() {
     myApp();
 
 };
-
