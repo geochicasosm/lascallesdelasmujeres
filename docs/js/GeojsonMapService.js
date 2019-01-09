44001d3f7d799c99f174895462362e0d05430d0e
tammypmc <tammymoscoso2@gmail.com>
2019-01-09 16:25:49 -0600

    cambios en la url para la rama develop

--- docs/js/GeojsonMapService.js
+++ docs/js/GeojsonMapService.js
@@ -1,7 +1,7 @@
 function GeojsonMapService(){
 
     //this.map = map;
-    this.urlData = 'https://raw.githubusercontent.com/labexp/lascallesdelasmujeres/master';
+    this.urlData = 'https://raw.githubusercontent.com/labexp/lascallesdelasmujeres/develop';
 
     this.loadGeojson = function(map, folder, isMobile, coords){
 
